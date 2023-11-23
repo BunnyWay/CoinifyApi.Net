9c65d5afb93a252047acd3fdbf3a3c3fbeffbe12
Željko Baranek <zeljko@bunny.net>
2023-11-23 01:30:45 +0100

    fix: example project add api url sandbox

--- CoinifyApi.Example/Program.cs
+++ CoinifyApi.Example/Program.cs
@@ -14,7 +14,8 @@ namespace CoinifyApi.Example
         {
             var coinify = new Coinify(
                 "1wCeblZxlWGryO7WrXLnkfUU1XJMFhphiw7d1jG8ykj92AC+ZgzoqCjgYpqJzDKd",
-                "EmefRmd9d4vw4xCgSXPWGIjBWWbAoak9HXxcDTvrUaz9L0nsspztN77eXytFF5r3");
+                "EmefRmd9d4vw4xCgSXPWGIjBWWbAoak9HXxcDTvrUaz9L0nsspztN77eXytFF5r3",
+                "https://api.sandbox.coinify.com/v3/");
 
             Console.ReadLine();
         }
